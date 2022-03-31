b3340386d9590f34f66038a037f8c58e78bd1470
SeyoungCho <wnl383@naver.com>
2022-03-31 23:37:44 +0900

    implemented redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects(){
+    return [
+      {
+        source:"/contact",
+        destination:"https://nomadcoders.co",
+        permanent:false,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
