6c4f32f8bdc58308815b9dd1f9a97a6fe16c2fb0
SeyoungCho <wnl383@naver.com>
2022-04-19 00:32:56 +0900

    implement ssr

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home({results}){
- 
+ const router = useRouter();
+ const onClick = (id, title) =>{
+   router.push(`/movies/${title}/${id}`);
+ }
   return (
     <div className="container">
       <Seo title="Home" />
-      
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div onClick={()=>{onClick(movie.id, movie.title)}} className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
@@ -44,7 +52,7 @@ export default function Home({results}){
   );
 }
 
-export async function getServerSideProps(){
+export async function getServerSideProps(context){
   /* This code only runs on the server */
   /* Never happens in the client side */
   const { results } = await (
